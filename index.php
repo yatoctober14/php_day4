2dae0a3e1c6be83386d44506ca5e978a68577f40
AbdulnaserMohsen <naserahmed1995@gmail.com>
2020-12-16 21:09:55 +0200

    2commit

--- index.php
+++ index.php
@@ -93,10 +93,21 @@
 		}
 		
 	}
-
-
-
-
+	
+	
+	//array pointers
+	echo "<br>";
+	$arr = [5,6,1,6,4,1,2,9];
+	echo(current($arr))."<br>";
+	echo next($arr)."<br>";
+	echo next($arr)."<br>";
+	echo current($arr)."<br>";
+	echo prev($arr)."<br>";
+	echo current($arr)."<br>";
+	echo end($arr)."<br>";
+	echo current($arr)."<br>";
+	echo reset($arr)."<br>";
+	echo current($arr)."<br>";
 
 
 
